7d6da779d0ffaa091fbc64a54c023bc79d7b0862
Somsubhra Bairi <somsubhra.bairi@gmail.com>
2014-12-28 20:33:03 +0530

    Basic client server

---
+++ client.py
@@ -0,0 +1,60 @@
+# Pushy example client
+# Written by Somsubhra
+
+# All imports
+import socket, select, string, sys
+ 
+ # Message prompt
+def prompt():
+  sys.stdout.write(">")
+  sys.stdout.flush()
+
+# The main method of the program
+def main():
+  if(len(sys.argv) < 3) :
+    print 'Usage : python client.py <hostname> <port>'
+    sys.exit()
+     
+  host = sys.argv[1]
+  port = int(sys.argv[2])
+     
+  s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+  s.settimeout(2)
+     
+  # Connect to Pushy server
+  try :
+    s.connect((host, port))
+  except :
+    print 'Unable to connect'
+    sys.exit()
+     
+  print 'Connected to Pushy server. Start sending messages\n'
+  prompt()
+     
+  while True:
+
+    socket_list = [sys.stdin, s]
+         
+    # Get the list sockets which are readable
+    read_sockets, write_sockets, error_sockets = select.select(socket_list , [], [])
+         
+    for sock in read_sockets:
+      
+      # Incoming message from Pushy server
+      if sock == s:
+        data = sock.recv(4096)
+        if not data :
+          print '\nDisconnected from Pushy server'
+          sys.exit()
+        else :
+          sys.stdout.write(data)
+          prompt()
+             
+      else :
+        # Send message to Pushy server
+        msg = sys.stdin.readline()
+        s.send(msg)
+        prompt()
+
+if __name__ == "__main__":
+  main()
---
+++ pushy.py
@@ -0,0 +1,50 @@
+# Pushy push server
+# Written by Somsubhra
+
+# All imports
+import socket, select
+
+# The main method of the program
+def main():
+  
+  # Socket descriptors
+  connection_list = []
+  recv_buffer = 4096
+  port = 5000
+
+  server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+  server_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+  server_socket.bind(("0.0.0.0", port))
+  server_socket.listen(10)
+
+  connection_list.append(server_socket)
+
+  print "Pushy listening on port " + str(port)
+
+  while True:
+
+    read_sockets, write_sockets, error_sockets = select.select(connection_list, [], [])
+
+    for sock in read_sockets:
+
+      if sock == server_socket:
+        # Accept incoming connections to server
+        sockfd, addr = server_socket.accept()
+        connection_list.append(sockfd)
+        print "Client (%s %s) connected to Pushy" % addr
+      else:
+        # Process data from clients
+        try:
+          data = sock.recv(recv_buffer)
+          if data:
+            print "New message from" + str(sock.getpeername()) + ": " + data
+        except:
+          print "Client (%s, %s) disconnected from Pushy" % addr
+          sock.close()
+          connection_list.remove(sock)
+          continue
+
+  server_socket.close()
+
+if __name__ == "__main__":
+  main()
