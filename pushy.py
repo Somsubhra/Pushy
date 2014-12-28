317ba856d433edc7c3973df686362f3279e7839b
Somsubhra Bairi <somsubhra.bairi@gmail.com>
2014-12-29 00:00:33 +0530

    OOPS

--- pushy.py
+++ pushy.py
@@ -6,70 +6,89 @@ import socket
 import select
 import sqlite3
 
-# Return whether the message
-# is a command or not
-def is_command(message):
-  return message[0] == '/'
+
+class Pushy:
+
+
+  # Constructor for the Pushy server
+  def __init__(self, host, port, recv_buffer_size):
+
+    self.host = host
+    self.port = port
+    self.recv_buffer_size = recv_buffer_size
+
+    # Setup database
+    try:
+      self.db = sqlite3.connect('pushy_data.db')
+      self.db_cursor = db.cursor()
+      query = '''
+        CREATE TABLE IF NOT EXISTS channel (
+          id INTEGER PRIMARY KEY,
+          name TEXT NOT NULL
+        )
+      '''
+      self.db_cursor.execute(query)
+      self.db.commit()
+    except Exception as e:
+      self.db.rollback()
+    finally:
+      self.db.close()
+
+    # Set up Pushy server's socket
+    self.connection_list = []
+
+    self.server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    self.server_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+    self.server_socket.bind((self.host, self.port))
+    self.server_socket.listen(10)
+
+    self.connection_list.append(self.server_socket)
+
+
+  # The main loop of Pushy server
+  def run(self):
+    print "Pushy listening on port " + str(self.port)
+
+    while True:
+
+      read_sockets, write_sockets, error_sockets = select.select(self.connection_list, [], [])
+
+      for sock in read_sockets:
+
+        if sock == self.server_socket:
+
+          # Accept incoming connections to server
+          sockfd, addr = self.server_socket.accept()
+          self.connection_list.append(sockfd)
+          print "Client (%s %s) connected to Pushy" % addr
+
+        else:
+
+          # Process data from clients
+          try:
+            data = sock.recv(self.recv_buffer_size)
+            if data:
+              print "New message from" + str(sock.getpeername()) + ": " + data
+              print self.is_command(data)
+
+          except:
+            print "Client (%s, %s) disconnected from Pushy" % addr
+            sock.close()
+            self.connection_list.remove(sock)
+            continue
+
+    self.server_socket.close()
+
+  # Return whether the message is a command or not
+  def is_command(self, message):
+    return message[0] == '/'
+
 
 # The main method of the program
 def main():
+  pushy_server = Pushy("0.0.0.0", 5000, 4096)
+  pushy_server.run()
 
-  # Setup database
-  try:
-    db = sqlite3.connect('pushy_data.db')
-    db_cursor = db.cursor()
-    query = '''
-      CREATE TABLE IF NOT EXISTS channel (
-        id INTEGER PRIMARY KEY,
-        name TEXT NOT NULL
-      )
-    '''
-    db_cursor.execute(query)
-    db.commit()
-  except Exception as e:
-    db.rollback()
-  finally:
-    db.close()
-
-  # Socket descriptors
-  connection_list = []
-  recv_buffer = 4096
-  port = 5000
-
-  server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-  server_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-  server_socket.bind(("0.0.0.0", port))
-  server_socket.listen(10)
-
-  connection_list.append(server_socket)
-
-  print "Pushy listening on port " + str(port)
-
-  while True:
-
-    read_sockets, write_sockets, error_sockets = select.select(connection_list, [], [])
-
-    for sock in read_sockets:
-
-      if sock == server_socket:
-        # Accept incoming connections to server
-        sockfd, addr = server_socket.accept()
-        connection_list.append(sockfd)
-        print "Client (%s %s) connected to Pushy" % addr
-      else:
-        # Process data from clients
-        try:
-          data = sock.recv(recv_buffer)
-          if data:
-            print "New message from" + str(sock.getpeername()) + ": " + data
-
-        except:
-          print "Client (%s, %s) disconnected from Pushy" % addr
-          sock.close()
-          connection_list.remove(sock)
-          continue
-
-  server_socket.close()
 
 if __name__ == "__main__":
   main()
